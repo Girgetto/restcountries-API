e9613a94a9b412dbdef2f79c190eb9da1be9e7eb
PabloValdazoDEV <157512231+PabloValdazoDEV@users.noreply.github.com>
2024-10-24 21:17:36 +0200

    Performance filtro API

--- index.js
+++ index.js
@@ -1,53 +1,55 @@
+const btnSubmit = document.getElementById("formulario");
+const countriesNode = document.getElementById("countries");
+const urlApiCount = "https://restcountries.com/v3.1/all";
+const arrayCountry = []
+
+//Conexión con la API y guardamos la respues en un ARRAY
+fetch(urlApiCount)
+  .then( (response) => response.json())
+  .then( (countries) => {
+    countries.forEach((el)=>{
+      arrayCountry.push(el)
+      
+    })
+    addHTML(arrayCountry)
+  });
+
 const cardTemplate = (pais) => {
   const { name, flags } = pais;
   return `<img id="flag-image" src="${flags.png}" alt="flag" />
               <h1 class="center">${name.common}</h1>`;
 };
 
-const countriesNode = document.getElementById("countries");
-
-const urlApiCount = "https://restcountries.com/v3.1/all";
-
-fetch(urlApiCount)
-  .then(function (response) {
-    return (json = response.json());
-  })
-  .then(function (countries) {
-    countries.forEach((country) => {
-      const respuesta = cardTemplate(country);
-      const div = document.createElement("div");
-      div.className = "card";
-      div.innerHTML = respuesta;
-      countriesNode.appendChild(div);
-    });
-  });
-
 const filtrar = (e) => {
   e.preventDefault();
   const valorSelect = e.target[0].value;
-  let url;
-  valorSelect === "Todos" ? url = "all" : url = 'region/' + valorSelect
-  filtrarApi(url)
+  console.log(valorSelect)
+  filtrarArray(valorSelect)
 };
-const btnSubmit = document.getElementById("formulario");
-btnSubmit.addEventListener("submit", filtrar);
 
-
-const filtrarApi = async(url) =>{
-  countriesNode.innerHTML = ""
-  const urlCustom = `https://restcountries.com/v3.1/${url}`
-  fetch(urlCustom)
-  .then(function (response) {
-    return (json = response.json());
-  })
-  .then(function (countries) {
-    countries.forEach((country) => {
-      const respuesta = cardTemplate(country);
-      const div = document.createElement("div");
-      div.className = "card";
-      div.innerHTML = respuesta;
-      countriesNode.appendChild(div);
-    });
-  });
-  
+const filtrarArray = (filtro) => {
+if(filtro === "Todos" || !filtro){
+  addHTML(arrayCountry);
+  return 
+}else{
+  const arrayFiltrado = arrayCountry.filter((countryFilter)=> countryFilter.region == filtro )
+  addHTML(arrayFiltrado)
 }
+ 
+}
+
+const addHTML = (filtro)=>{
+  console.log(filtro)
+  countriesNode.innerHTML = ''
+  filtro.forEach((country) => {
+    const respuesta = cardTemplate(country);
+    const div = document.createElement("div");
+    div.className = "card";
+    div.innerHTML = respuesta;
+    countriesNode.appendChild(div);
+  });
+
+}
+
+
+btnSubmit.addEventListener("submit", filtrar);
