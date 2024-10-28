1074bc735eefa9856ef14d24f87ecd3b1b957a3c
2how <24274717+2how@users.noreply.github.com>
2024-10-28 20:45:11 +0100

    added continent filter

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cardTemplate = function (flagUrl, countryName) {
 };
 
 const countriesNode = document.getElementById("countries");
+let countriesObject;
 
 fetch("https://restcountries.com/v3.1/all")
   .then(function (response) {
@@ -15,7 +16,29 @@ fetch("https://restcountries.com/v3.1/all")
     return response.json();
   })
   .then(function (countries) {
+    countriesObject = countries;
+    
     countries.map((country) => {
       countriesNode.innerHTML += cardTemplate(country.flags.svg, country.name.common);
     })
   });
+
+document.getElementById("continent").addEventListener("change", function() {
+
+  if (countriesObject != undefined) {
+    const continent = this.value;
+    countriesNode.innerHTML = "";
+    if (continent != "") {
+      countriesObject.map((country) => {
+        if (country.continents.some((element) => element.toLowerCase() == continent.toLowerCase())) {
+          countriesNode.innerHTML += cardTemplate(country.flags.svg, country.name.common);
+        }
+      })
+    } else {
+      countriesObject.map((country) => {
+        countriesNode.innerHTML += cardTemplate(country.flags.svg, country.name.common);
+      })
+    }
+  }
+
+});
